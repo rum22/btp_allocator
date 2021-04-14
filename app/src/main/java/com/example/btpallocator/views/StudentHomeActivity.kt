5f73f0ad9e5eb5c0ef28d4bb99c2e6b6acaac0d6
dev_tanmay <bansaltanmay9@gmail.com>
2021-04-15 03:26:14 +0530

    connected add_project to prof_home and fill_pref to student_home

---
+++ app/src/main/java/com/example/btpallocator/views/StudentHomeActivity.kt
@@ -0,0 +1,22 @@
+package com.example.btpallocator.views
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.btpallocator.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_student_home.*
+
+class StudentHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_home)
+        initViews()
+    }
+    private fun initViews() {
+        btnFill_preferences.setOnClickListener {
+            val studentPrefIntent = Intent(this@StudentHomeActivity, FillPreferenceActivity::class.java)
+            startActivity(studentPrefIntent)
+        }
+    }
+}
