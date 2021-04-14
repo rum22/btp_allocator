b01a950d4d6a05b56f3225fa0d73fc93ffcc96e5
akhilchoudhary2k <akhil.choudhary2000@gmail.com>
2021-04-15 01:32:36 +0530

    Worked on connecting prof login and register Intent

--- app/src/main/java/com/example/btpallocator/views/MainActivity.kt
+++ app/src/main/java/com/example/btpallocator/views/MainActivity.kt
@@ -23,5 +23,21 @@ class MainActivity : AppCompatActivity() {
             val studentRegisterIntent = Intent(this@MainActivity, StudentRegisterActivity::class.java)
             startActivity(studentRegisterIntent)
         }
+
+        btnProfLogin.setOnClickListener {
+            val profLoginIntent = Intent(this@MainActivity, ProfLoginActivity::class.java)
+            startActivity(profLoginIntent)
+        }
+
+        btnProfRegister.setOnClickListener {
+            val profRegisterIntent = Intent (this@MainActivity, ProfRegisterActivity::class.java)
+            startActivity(profRegisterIntent)
+        }
+
+        btnAdminLogin.setOnClickListener {
+            val adminLoginIntent = Intent ( this@MainActivity, AdminLoginActivity::class.java)
+            startActivity(adminLoginIntent)
+        }
+
     }
 }
