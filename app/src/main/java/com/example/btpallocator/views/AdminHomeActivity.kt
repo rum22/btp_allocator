02f482b0e552df28fd254eb5a60dc21394740c75
dev_tanmay <bansaltanmay9@gmail.com>
2021-04-15 12:44:47 +0530

    complete admin home redirections

--- app/src/main/java/com/example/btpallocator/views/AdminHomeActivity.kt
+++ app/src/main/java/com/example/btpallocator/views/AdminHomeActivity.kt
@@ -4,13 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.btpallocator.R
+import kotlinx.android.synthetic.main.activity_admin_home.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_student_login.*
 
 class AdminHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
+        initViews()
+    }
 
+    private fun initViews() {
+        allocat_projects.setOnClickListener {
+            val allocatIntent = Intent(this@AdminHomeActivity, AdminViewAllocationActivity::class.java)
+            startActivity(allocatIntent)
+        }
+
+        view_allocation.setOnClickListener {
+            val viewAllocatIntent = Intent(this@AdminHomeActivity, AdminViewAllocationActivity::class.java)
+            startActivity(viewAllocatIntent)
+        }
+
+        view_students.setOnClickListener {
+            val viewStudentIntent = Intent(this@AdminHomeActivity, AdminViewStudentActivity::class.java)
+            startActivity(viewStudentIntent)
+        }
     }
 
 }
